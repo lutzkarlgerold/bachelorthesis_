21731e06bbbae44787f7bbbe19d1f992a3644921
Lukas Hennies <45569834+Gornoka@users.noreply.github.com>
2021-05-27 19:20:48 +0200

    Update diff_ordner.py

--- diff_ordner.py
+++ diff_ordner.py
@@ -9,59 +9,33 @@ import os
 
 # Verzeichnis der Bilder
 
-def difference(Probe="004"):
+def difference(Probe="004",n_images_max):
     bild_format = "{}-{}-"
-
-    Datei1 = bild_format.format(Probe, 0)
-    Datei2 = bild_format.format(Probe, 1)
-    Datei3 = bild_format.format(Probe, 2)
-    Datei4 = bild_format.format(Probe, 3)
-    Datei5 = bild_format.format(Probe, 4)
-    Datei6 = bild_format.format(Probe, 5)
-    Datei7 = bild_format.format(Probe, 6)
-    Datei8 = bild_format.format(Probe, 7)
-    Datei9 = bild_format.format(Probe, 8)
-
+    bilder = []
+    
+    for i in range(9):
+        try:
+            bild_format.format(Probe,n_images_max +1)
+        except FileNotFoundError as F_e:
+            if i == 0:
+                raise F_e
+            else:
+                break
+                print(f"found {i+1} images for {Probe}")
 
     i = 1
 
     for x in range(0, 8):
         img_count = str(i)
         i = i + 1
-
-        img1 = Image.open(Datei1 + img_count + ".png")
-        img2 = Image.open(Datei2 + img_count + ".png")
-        img3 = Image.open(Datei3 + img_count + ".png")
-        img4 = Image.open(Datei4 + img_count + ".png")
-        img5 = Image.open(Datei5 + img_count + ".png")
-        img6 = Image.open(Datei6 + img_count + ".png")
-        img7 = Image.open(Datei7 + img_count + ".png")
-        img8 = Image.open(Datei8 + img_count + ".png")
-        img9 = Image.open(Datei9 + img_count + ".png")
-        diff = ImageChops.difference(img1, img2)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei2 + img_count + "-diff.png")
-        diff = ImageChops.difference(img1, img3)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei3 + img_count + "-diff.png")
-        diff = ImageChops.difference(img1, img4)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei4 + img_count + "-diff.png")
-        diff = ImageChops.difference(img1, img5)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei5 + img_count + "-diff.png")
-        diff = ImageChops.difference(img1, img6)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei6 + img_count + "-diff.png")
-        diff = ImageChops.difference(img1, img7)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei7 + img_count + "-diff.png")
-        diff = ImageChops.difference(img1, img8)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei8 + img_count + "-diff.png")
-        diff = ImageChops.difference(img1, img9)
-        if diff.getbbox():
-            diff.save("./diff/" + Datei9 + img_count + "-diff.png")
+        images = []
+        for i_p in bilder:
+            images.append(Image.open(Datei1 + img_count + ".png"))
+            
+        for image in images[1:]:
+            diff = ImageChops.difference(images[0], image)
+            if diff.getbbox():
+                diff.save("./diff/" + Datei2 + img_count + "-diff.png")
 
 if __name__ == '__main__':
 
