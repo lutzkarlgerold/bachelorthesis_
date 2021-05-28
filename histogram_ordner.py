a3014bee8cdc552e973a028345b1fcd35be169bb
Lutz Gerold <l.gerold@student.fontys.nl>
2021-05-28 15:14:30 +0200

    Header Noten angepasst (Summe der Datensätze) & Script "aufgeräumt"

--- histogram_ordner.py
+++ histogram_ordner.py
@@ -14,7 +14,6 @@ from scipy import stats
 Folder = os.chdir("C:/Users/lg/Dokumente/BA/bachelorthesis/input_pictures/Gruppe1/diff")
 
 # Anzahl der png-Dateien im Ordner zählen
-#Folder = "C:\\"
 Counter = 0
 Files = os.listdir(Folder)
 
@@ -23,37 +22,34 @@ for File in Files:
     if Ext == ".png":
         Counter += 1
 
-# Ort der Liste mit Noten
-# grades = ("C:/Users/lg/Dokumente/BA/bachelorthesis/Notenliste/grades.txt")
-
-# Ort der Outputdaten
-# output = ("C:/Users/lg/Dokumente/BA/004-013 Erste Serie für NN/Test 5 Histowerte via Py/output_dir")
-
 # Output-Noten als Dictionary einlesen
 with  open("C:/Users/lg/Dokumente/BA/bachelorthesis/Notenliste/grades.txt", "r") as f:
     rows = ( line.rstrip('\n').split('\t') for line in f )
     d = { row[0]:row[1] for row in rows }
     print(d)
 
-# Header des Daten-Files erstellen        
+# Header des Daten-Files erstellen (Summe der Datensätze)
 with open('input_daten.txt', 'a', newline='') as f_output:
     csv_output = csv.writer(f_output, delimiter=' ')
     csv_output.writerow([int(Counter)])
 
-# Header des Noten-Files erstellen
+# Header des Noten-Files erstellen (Summe der Datensätze)
 with open('input_noten.txt', 'a', newline='') as f_output:
     csv_output = csv.writer(f_output, delimiter=' ')
     csv_output.writerow([int(Counter)])
-    
+
+# Dateiname wird ausgegeben
 for file in glob.glob("*.png"):
     print (os.path.basename(file))
     sample = os.path.basename(file)
     file[0]
-    samplename = file[0]+file[1]+file[2]+file[3]+file[4]
+#    samplename = file[0]+file[1]+file[2]+file[3]+file[4]
 #    print (samplename)
 
+# Bildausschnitt wird geladen
     image0 = cv2.imread(file)
 
+# Variable für Histogramm wird erzeugt
     hist = image0
     
     mode = stats.mode(hist)
@@ -63,6 +59,7 @@ for file in glob.glob("*.png"):
     with open('input_daten.txt', 'a', newline='') as f_output:
         csv_output = csv.writer(f_output, delimiter=' ')
         csv_output.writerow([int(np.mean(hist)), int(np.std(hist)), np.max(hist), format(total_mode.mode[0])])
+    # Ausgabe aller Histogramm-Werte
     print ("Mean = {:.1f}, standard deviation = {:.1f}, Count = {:.0f}, min = {:.0f}, max = {:.0f}".format(
         np.mean(hist).item(),
         np.std(hist).item(),
@@ -76,7 +73,6 @@ for file in glob.glob("*.png"):
     # Noten aus Dictionary in TXT-File schreiben
     dict = d
     sn = dict.get(samplename)
-#    float(sn)
     print ("Note: %s" % sn)
 
     with open('input_noten.txt', 'a', newline='') as f_output:
