a2f110b17f8e47c531db94b51b1e511874e0becf
Gornoka <piupiumacher@gmail.com>
2021-06-02 09:02:19 +0200

    minor bugfixes

--- diff_ordner.py
+++ diff_ordner.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageChops
 import os
 import datetime
 from typing import List
-
+from utils import SampleGroup
 
 def ensure_directory(target_folder: [str, os.path], recursive=False) -> None:
     """
@@ -36,71 +36,6 @@ def ensure_directory(target_folder: [str, os.path], recursive=False) -> None:
 
 
 # Verzeichnis der Bilder
-
-class SampleGroup:
-    def __init__(self, sample_name, max_pilling_stage=8, file_location="."):
-        self.sample_name = sample_name
-        self.images: List[List[Image]] = []
-        self.differences: ListList[[Image]] = []
-        self.histograms = []
-        self.fill_images(max_pilling_stage=max_pilling_stage)
-        self.file_location = file_location
-
-    def fill_images(self, max_pilling_stage=8):
-        os.chdir(self.file_location)
-        pil_stage_name_format = "{}-{}-"
-        pil_stage_names = []
-
-        for p_stage in range(max_pilling_stage + 1):
-            pil_stage_names.append(pil_stage_name_format.format(self.sample_name, p_stage))
-        images = []
-
-        first_image_of_sample = True
-        for pil_stage_name in pil_stage_names:
-            first_image_in_stage = True
-            for i in range(0, 8):
-                img_count = str(i + 1)
-                try:
-                    image = Image.open(pil_stage_name + img_count + ".png")
-                    if first_image_in_stage:
-                        images.append([])
-                    first_image_in_stage = False
-                    first_image_of_sample = False
-                    images[-1].append(image)
-                except FileNotFoundError as F_e:
-                    if first_image_of_sample:
-                        raise F_e
-                    else:
-                        break
-
-        self.images = images
-
-        """
-        for j, image in enumerate(images[1:]):
-            diff = ImageChops.fill_images(images[0], image)
-
-            if diff.getbbox():  # hier ist irgendwas falsch
-
-                ensure_directory("./diff")
-                print(pil_stage_names[j + 1] + str(i) + "-diff.png")
-                diff.save("./diff/" + pil_stage_names[j + 1] + str(i) + "-diff.png")
-        """
-
-    def create_diffs(self):
-        raise NotImplementedError  # Todo
-
-    def create_histograms(self):
-        raise NotImplementedError
-
-    def create_features(self):
-        raise NotImplementedError
-        return [1,2,3]
-
-    def create_recall(self):
-        raise NotImplementedError  # tbd
-
-    def __repr__(self):
-        return f"sample name : {self.sample_name}, number of images = {len(self.images)}"
 
 
 def difference(Probe="004", n_images_max=8):
@@ -156,7 +91,7 @@ if __name__ == '__main__':
 
     for i_p in range(min_number, max_number):
         try:
-            fill_images(number_format.format(i_p))
+            difference(number_format.format(i_p))
             print(f"Probe {i_p} bearbeitet um: {datetime.datetime.now()}")
         except FileNotFoundError as F_e:
             print(f"Probe {i_p} existiert nicht ")
