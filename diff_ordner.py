e4f2b55a1161dfab07e301eb5033f012f8027d79
Gornoka <piupiumacher@gmail.com>
2021-06-02 08:45:55 +0200

    reennabled exception handling

--- diff_ordner.py
+++ diff_ordner.py
@@ -8,6 +8,8 @@ import pathlib
 from PIL import Image, ImageChops
 import os
 import datetime
+from typing import List
+
 
 def ensure_directory(target_folder: [str, os.path], recursive=False) -> None:
     """
@@ -31,17 +33,69 @@ def ensure_directory(target_folder: [str, os.path], recursive=False) -> None:
                 os.mkdir(target_folder)
             except OSError:
                 logging.error(f"failed to create folder {target_folder}")
+
+
 # Verzeichnis der Bilder
+
+class SampleGroup:
+    def __init__(self, sample_name, max_pilling_stage=8):
+        self.sample_name = sample_name
+        self.images: List[List[Image]] = []
+        self.differences: ListList[[Image]] = []
+        self.fill_images(max_pilling_stage=max_pilling_stage)
+
+    def fill_images(self, max_pilling_stage=8):
+        pil_stage_name_format = "{}-{}-"
+        pil_stage_names = []
+
+        for p_stage in range(max_pilling_stage + 1):
+            pil_stage_names.append(pil_stage_name_format.format(self.sample_name, p_stage))
+        images = []
+
+        first_image_of_sample = True
+        for pil_stage_name in pil_stage_names:
+            first_image_in_stage = True
+            for i in range(0, 8):
+                img_count = str(i+1)
+                try:
+                    image = Image.open(pil_stage_name + img_count + ".png")
+                    if first_image_in_stage:
+                        images.append([])
+                    first_image_in_stage = False
+                    first_image_of_sample = False
+                    images[-1].append(image)
+                except FileNotFoundError as F_e:
+                    if first_image_of_sample:
+                        raise F_e
+                    else:
+                        break
+
+        self.images = images
+
+        """
+        for j, image in enumerate(images[1:]):
+            diff = ImageChops.fill_images(images[0], image)
+
+            if diff.getbbox():  # hier ist irgendwas falsch
+
+                ensure_directory("./diff")
+                print(pil_stage_names[j + 1] + str(i) + "-diff.png")
+                diff.save("./diff/" + pil_stage_names[j + 1] + str(i) + "-diff.png")
+        """
+
+    def create_diffs(self):
+        raise NotImplementedError # Todo
+
+    def __repr__(self):
+        return f"sample name : {self.sample_name}, number of images = {len(self.images)}"
+
 
 def difference(Probe="004", n_images_max=8):
     bild_format = "{}-{}-"
     bilder = []
-    
-    for i in range(n_images_max+1):
 
+    for i in range(n_images_max + 1):
         bilder.append(bild_format.format(Probe, i))
-
-
 
     i = 1
 
@@ -51,7 +105,6 @@ def difference(Probe="004", n_images_max=8):
         images = []
         first = True
         for image_name in bilder:
-
             try:
                 images.append(Image.open(image_name + img_count + ".png"))
                 first = False
@@ -60,22 +113,29 @@ def difference(Probe="004", n_images_max=8):
                     raise F_e
                 else:
                     break
+
         for j, image in enumerate(images[1:]):
             diff = ImageChops.difference(images[0], image)
 
             if diff.getbbox():  # hier ist irgendwas falsch
 
                 ensure_directory("./diff")
-                print(bilder[j+1] + str(i) + "-diff.png")
-                diff.save("./diff/" + bilder[j+1] + str(i) + "-diff.png")
+                print(bilder[j + 1] + str(i) + "-diff.png")
+                diff.save("./diff/" + bilder[j + 1] + str(i) + "-diff.png")
+
 
 if __name__ == '__main__':
-
     workdir = "./input_pictures/Gruppe1"
     os.chdir(workdir)
 
-    # difference("005")
-    min_number = 127
+    probe = SampleGroup("125")
+    print(probe)
+    print(probe.images)
+
+    """
+    
+    # fill_images("005")
+    min_number = 1
     max_number = 150
     workdir_path = pathlib.Path(".")
     print(workdir_path.iterdir())
@@ -84,9 +144,10 @@ if __name__ == '__main__':
 
     for i_p in range(min_number, max_number):
         try:
-            difference(number_format.format(i_p))
+            fill_images(number_format.format(i_p))
             print(f"Probe {i_p} bearbeitet um: {datetime.datetime.now()}")
         except FileNotFoundError as F_e:
             print(f"Probe {i_p} existiert nicht ")
     print(number_format.format(max_number))
     print(number_format.format(min_number))
+    """
